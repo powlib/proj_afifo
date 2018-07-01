3845603dcd6867fb09eec903d0da222784b78a39
Andrew Powell <andrewandrepowell2@gmail.com>
2018-06-30 20:28:17 -0500

    Changed the ignore file to ignore the launch scripts.

--- sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/debug_run_0.tcl
+++
@@ -1,9 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-targets -set -filter {jtag_cable_name =~ "Digilent Nexys4DDR 210292745510A" && level==0} -index 0
-fpga -file /workspace/git_ws/proj_afifo/sdk/top_hw_platform_0/top.bit
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292745510A"} -index 0
-rst -system
-after 3000
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292745510A"} -index 0
-dow /workspace/git_ws/proj_afifo/sdk/app/Debug/app.elf
-bpadd -addr &main
