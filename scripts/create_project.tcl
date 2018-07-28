33a4ca3defc4d91ef1f9fef8e4ebf3e15f2d6e78
Andrew Powell <andrewandrepowell2@gmail.com>
2018-07-28 11:24:58 -0500

    Dammmiit

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -102,7 +102,7 @@ if { $::argc > 0 } {
 }
 
 # Set the directory path for the original project from where this script was exported
-set orig_proj_dir "[file normalize "$origin_dir/../proj_vivado"]"
+set orig_proj_dir "[file normalize "$origin_dir/proj_vivado"]"
 
 # Create project
 create_project ${project_name} ./${project_name} -part xc7a100tcsg324-1
@@ -159,17 +159,17 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set 'sources_1' fileset object
 set obj [get_filesets sources_1]
 set files [list \
- "[file normalize "$origin_dir/../hdl/both/proj_lane.v"]"\
- "[file normalize "$origin_dir/../powlib/powlib_std.vh"]"\
- "[file normalize "$origin_dir/../powlib/powlib_fifo.v"]"\
- "[file normalize "$origin_dir/../powlib/powlib_std.v"]"\
- "[file normalize "$origin_dir/../hdl/both/proj_top.v"]"\
- "[file normalize "$origin_dir/../hdl/synth/xilinx_ip_wrapper.v"]"\
+ "[file normalize "$origin_dir/hdl/both/proj_lane.v"]"\
+ "[file normalize "$origin_dir/powlib/powlib_std.vh"]"\
+ "[file normalize "$origin_dir/powlib/powlib_fifo.v"]"\
+ "[file normalize "$origin_dir/powlib/powlib_std.v"]"\
+ "[file normalize "$origin_dir/hdl/both/proj_top.v"]"\
+ "[file normalize "$origin_dir/hdl/synth/xilinx_ip_wrapper.v"]"\
 ]
 add_files -norecurse -fileset $obj $files
 
 # Set 'sources_1' fileset file properties for remote files
-set file "$origin_dir/../hdl/both/proj_lane.v"
+set file "$origin_dir/hdl/both/proj_lane.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "Verilog" -objects $file_obj
@@ -182,7 +182,7 @@ set_property -name "used_in_implementation" -value "1" -objects $file_obj
 set_property -name "used_in_simulation" -value "1" -objects $file_obj
 set_property -name "used_in_synthesis" -value "1" -objects $file_obj
 
-set file "$origin_dir/../powlib/powlib_std.vh"
+set file "$origin_dir/powlib/powlib_std.vh"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "Verilog Header" -objects $file_obj
@@ -194,7 +194,7 @@ set_property -name "used_in" -value "synthesis simulation" -objects $file_obj
 set_property -name "used_in_simulation" -value "1" -objects $file_obj
 set_property -name "used_in_synthesis" -value "1" -objects $file_obj
 
-set file "$origin_dir/../powlib/powlib_fifo.v"
+set file "$origin_dir/powlib/powlib_fifo.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "Verilog" -objects $file_obj
@@ -207,7 +207,7 @@ set_property -name "used_in_implementation" -value "1" -objects $file_obj
 set_property -name "used_in_simulation" -value "1" -objects $file_obj
 set_property -name "used_in_synthesis" -value "1" -objects $file_obj
 
-set file "$origin_dir/../powlib/powlib_std.v"
+set file "$origin_dir/powlib/powlib_std.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "Verilog" -objects $file_obj
@@ -220,7 +220,7 @@ set_property -name "used_in_implementation" -value "1" -objects $file_obj
 set_property -name "used_in_simulation" -value "1" -objects $file_obj
 set_property -name "used_in_synthesis" -value "1" -objects $file_obj
 
-set file "$origin_dir/../hdl/both/proj_top.v"
+set file "$origin_dir/hdl/both/proj_top.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "Verilog" -objects $file_obj
@@ -233,7 +233,7 @@ set_property -name "used_in_implementation" -value "1" -objects $file_obj
 set_property -name "used_in_simulation" -value "1" -objects $file_obj
 set_property -name "used_in_synthesis" -value "1" -objects $file_obj
 
-set file "$origin_dir/../hdl/synth/xilinx_ip_wrapper.v"
+set file "$origin_dir/hdl/synth/xilinx_ip_wrapper.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "Verilog" -objects $file_obj
